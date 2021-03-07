d151911d472c6d6f2f52ef075577662e43f5af46
Bunei shadieh <buneishadrack5@gmail.com>
2021-03-07 10:57:34 +0300

    added codes  base.html file

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 export SQLALCHEMY_DATABASE_URI=postgresql+psycopg2://yvette:yvette@localhost/blog
-export MAIL_USERNAME=yvetteadan2000@gmail.com
-export MAIL_PASSWORD=yvette
+export MAIL_USERNAME=buneishadrack5@gmail.com
+export MAIL_PASSWORD=1@Shadrack
 export SECRET_KEY=ac7d66d2
-python3.6 manage.py server
+python3.8 manage.py server
